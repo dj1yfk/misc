6644a61520231b59c3042db04f34c780deaed88c
Fabian Kurz <fabian@fkurz.net>
2024-09-17 06:53:30 +0200

    WSPR overlay for QrssPiG output: Check time range of spots
    Previously, when there were no spots in the last 10 minute
    frame, the script simply took the last spots from the last
    10 minute frame that had any decode. Now the full time stamp
    of ALL_WSPR.TXT is checked.

--- wspr/annotate-qrss.pl
+++ wspr/annotate-qrss.pl
@@ -20,13 +20,12 @@ my $y0 = 215;
 my $ys = 1.4;
 
 chdir("/home/fabian/grabs");
-`rm -f 30m-*.png`;
 
 my $filename = "30m.png";
 $filename =~ /(.*).png$/;
 my $basename = $1;
 
-my $data = `tail -n150 ~/wsprcan/ALL_WSPR.TXT`;
+my $data = `tail -n300 ~/wsprcan/ALL_WSPR.TXT`;
 my @d = split(/\n/, $data);
 @d = reverse(@d);
 
@@ -40,27 +39,33 @@ my $annotations = "";
 
 # assemble string with annotations 
 
+# example input line:
+# 240810 1502   4 -23 -2.2 10.1401428  DJ2TS JO40 30           0     1    0
+
+my @gmt = gmtime();  # when we process the above (1500-1508 slots, it is 1510)
+my $time_filter = sprintf("%02d%02d", $gmt[2], $gmt[1] - 10);    # 1500
+$time_filter =~ s/\d$//g;    # 150
+
 foreach my $line (@d) {
-    # 240810 1502   4 -23 -2.2 10.1401428  DJ2TS JO40 30           0     1    0
-    $cnt++;
-    my $oc = $cnt - 1;
     my @a = split(/\s+/, $line);
     my $slot = substr($a[1], 3, 1);    # 0, 2, 4, 6, 8 ...
-    my $min10 = substr($a[1], 2, 1);
+    my $min10 = substr($a[1], 0, 3);   # 150
     my $freq = $a[5]*1000000;
     my $call = $a[6]." (".$a[3]." dB)";
 
-    if ($min10_start == -1) {
-        $min10_start = $min10;
+    if ($min10 ne $time_filter) {
+        last;
     }
 
-    if ($min10 != $min10_start) {
-        $cnt--;
-        last;
+    # don't print decodes above 10.140275 because they are out of the spectrum
+    # display
+    if ($freq > 10140275) {
+        next;
     }
 
     my $xpos = 4 + $x0 + $wx * $slot;
     my $ypos = $y0 - $ys * ($freq - 10140200) - 5;
+
     $annotations .= " -annotate +$xpos+$ypos '$call'  ";
 
 }
